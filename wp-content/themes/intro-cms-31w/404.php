4d4df142625837d118c76655a396d3f104eaba05
Martin Tremblay <mart.tremblay.mt5@gmail.com>
2023-02-15 20:45:36 -0500

    c4 - Création de la page 404

---
+++ wp-content/themes/intro-cms-31w/404.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Modèle 404.php représente le modèle utiliser pour informer qu'une erreur de type 404 est survenue
+ */
+
+get_header() ?>
+<main>
+    <code>404.php</code>
+    <h1>Erreur 404 - la page n'existe pas.</h1>
+</main>
+<?php get_footer(); ?>
