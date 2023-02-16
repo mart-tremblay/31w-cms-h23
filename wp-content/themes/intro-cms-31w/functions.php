7acc6591c42eeb8e6246a6d317936a2b70e46527
Martin Tremblay <mart.tremblay.mt5@gmail.com>
2023-02-15 20:49:15 -0500

    c4 - Dans la page functions.php ajout de register_menus et add_theme_support pour le titre principal du site

--- wp-content/themes/intro-cms-31w/functions.php
+++ wp-content/themes/intro-cms-31w/functions.php
@@ -1,11 +1,41 @@
 <?php
 
-// Enfiler la feuille de style
-function ajouter_styles() {
-	wp_enqueue_style( '31w-style-principal', // id de la feuille de style
-		get_template_directory_uri() . '/style.css', // adresse url de la feuille de style
-		array(), // les dépendances avec les autres feuilles de style
-		filemtime( get_template_directory() . '/style.css' ) ); // la de la dernière feuille de style
-}
+	// Enfiler la feuille de style
+	function ajouter_styles()
+	{
+		wp_enqueue_style( '31w-style-principal', // id de la feuille de style
+			get_template_directory_uri() . '/style.css', // adresse url de la feuille de style
+			array(), // les dépendances avec les autres feuilles de style
+			filemtime( get_template_directory() . '/style.css' ) ); // la de la dernière feuille de style
+	}
 
-add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
+	add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
+
+	// Ajout d'options complémentaires pour le site (add_theme_support)
+	add_theme_support(
+		'html5',
+		array('search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption')
+	);
+
+	add_theme_support( 'title-tag' );
+
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height' => 150,
+			'width'  => 150)
+	);
+
+	// Enregistrement de menus (register_nav_menus)
+	function enregistrement_des_menus()
+	{
+		register_nav_menus( array(
+			'menu_entete' => 'Menu entête' ,
+			'menu_pied_de_page'  => 'Menu pied de page',
+		) );
+	}
+	add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
